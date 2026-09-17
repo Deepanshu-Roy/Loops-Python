64a695cf4df723d4cb7edc9ed5c393229d1cf9e1
Deepanshu-Roy <deepanshuroy1811@gmail.com>
2026-09-17 18:55:03 +0530

    New Update in Loop

--- Patterns.py
+++ Patterns.py
@@ -1,5 +1,11 @@
-for i in range(5):
-    print("*", end=' ')
-    for j in range(i):
+print("Press 1: To Run the pattern")
+print("Press 2: For closing the program")
+con=int(input("Press Either 1/2: "))
+if con==1:
+    for i in range(5):
         print("*", end=' ')
-    print()
+        for j in range(i):
+            print("*", end=' ')
+        print()
+elif con==2:
+    print("Program Ended. Thank You!")
