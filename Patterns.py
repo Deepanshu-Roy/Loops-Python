1da505bea37ee2a8a92ad44f930510534c768ee6
Deepanshu-Roy <deepanshuroy1811@gmail.com>
2026-09-17 22:34:00 +0530

    Password Feature Added

--- Patterns.py
+++ Patterns.py
@@ -1,23 +1,31 @@
-# print("Press 1: To Run the pattern")
-# print("Press 2: For closing the program")
-# con=int(input("Press Either 1/2: "))
-# if con==1:
-for i in range(4):
-    print("*", end=' ')
-    for j in range(i):
-        print("*", end=' ')
-    print()
+passw="Dr_18112009"
+for l in range(3):
+    enter=input("Enter Password: ")
+    if passw==enter:
+        print("Press 1: To Run the pattern")
+        print("Press 2: For closing the program")
+        con=int(input("Press Either 1/2: "))
+        if con==1:
+            for i in range(4):
+                print("*", end=' ')
+                for j in range(i):
+                    print("*", end=' ')
+                print()
 
-for a in range(3):
-    print("*", end=' ')
-    for b in range(4):
-        print("*", end=' ')
-    print()
+            for a in range(3):
+                print("*", end=' ')
+                for b in range(4):
+                    print("*", end=' ')
+                print()
 
-for k in range(4,0,-1):
-    print("*", end=' ')
-    for h in range(k):
-        print("*", end=' ')
-    print()
-# elif con==2:
-#     print("Program Ended. Thank You!")
+            for k in range(4,0,-1):
+                print("*", end=' ')
+                for h in range(k):
+                    print("*", end=' ')
+                print()
+        elif con==2:
+            print("Program Ended. Thank You!")
+        break
+    else:
+        print("Wrong Password!")
+        print("Try Again")
