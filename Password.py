513b36766741b5f8b252e2075b76fa8bca2aec8f
Deepanshu-Roy <deepanshuroy1811@gmail.com>
2026-09-17 22:26:18 +0530

    Password Python code

---
+++ Password.py
@@ -0,0 +1,12 @@
+passw="Dr_18112009"
+for l in range(3):
+    enter=input("Enter Password: ")
+    if passw==enter:
+
+        # Add which comands you want to run under this password
+        print("Done!")
+
+        break
+    else:
+            print("Wrong Password!")
+            print("Try Again")
